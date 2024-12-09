b24f4d8c51761cb70e1065540f7dd155b7c0a7db
Enterdatprisonfor445years <85706754+Enterdatprisonfor445years@users.noreply.github.com>
2024-12-09 17:21:57 -0500

    laser and collision
    the collisions are unfinished because i haven't assigned my VFX variables or created my VFX objects.

--- Car Crashers/Assets/Scripts/Player Movement.cs
+++ Car Crashers/Assets/Scripts/Player Movement.cs
@@ -1,13 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float controlSpeed = 10f;
     [SerializeField] float xRange = 3f;
+    
+    [SerializeField] GameObject[] lasers;
 
     void Update()
+    {
+        ProcessTranslation();
+        ProcessFiring();
+    }
+
+    private void ProcessTranslation()
     {
         float xThrow = Input.GetAxis("Horizontal");
         float xOffset = xThrow * Time.deltaTime * controlSpeed;
@@ -15,5 +24,26 @@ public class PlayerMovement : MonoBehaviour
         float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
 
         transform.localPosition = new Vector3(clampedXPos, transform.localPosition.y, transform.localPosition.z);
+    }
+
+    void ProcessFiring()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            SetLaserActive(true);
+        }
+        else
+        {
+            SetLaserActive(false);
+        }
+    }
+
+    void SetLaserActive(bool isActive)
+    {
+        foreach (GameObject laser in lasers)
+        {
+            var emissionModule = laser.GetComponent<ParticleSystem>().emission;
+            emissionModule.enabled = isActive;
+        }
     }
 }
